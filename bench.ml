482301a6d4ded8ea1a0c27fcdeedbc3355a1707c
yuki@is <iiyuki67@gmail.com>
2017-12-12 15:03:59 +0900

    bench.mlをApp.mlの変化に対応させた

--- bench.ml
+++ bench.ml
@@ -3,8 +3,8 @@ open Core
 open Operand
 
 let () =
-  Sim.file "test/sum.input";
-  !g_core.reg.(regnum_of_string "$a0") <- 200000000;
-  let core =  Sim.exec_all () in
+  App.load_file "test/sum.input";
+  App.set_args [20000000];
+  let core =  App.execute () in
   print_int !(core.count);
   print_newline ()
